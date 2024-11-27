5ad353561b4e7ec0ed45bed4955bd06fdb3557f4
fernandofatech <fernando.tech@icloud.com>
2024-11-27 02:57:01 -0300

    Refactor PostCSS configuration, remove deprecated file, update Tailwind CSS setup, and modify global styles for improved consistency

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,11 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // Inclui todos os arquivos JSX/TSX
+    "./src/app/**/*.{js,ts,jsx,tsx}", // Especificamente para a pasta app
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {}, // Adicione customizações aqui, se necessário
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [], // Adicione plugins aqui, se necessário
+};
